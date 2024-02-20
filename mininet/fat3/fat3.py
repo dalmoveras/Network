60ae7429d4d2d82d2fa64916610e119d72a6f327
dalmoveras <d.verasnt@gmail.com>
2024-02-19 23:48:07 -0800

    finish connections

--- mininet/fat3/fat3.py
+++ mininet/fat3/fat3.py
@@ -44,31 +44,40 @@ class Fat3(mininet.topo.Topo):
         self._aggregation_init(self.total_aggregation_switches)
         self._edge_init(self.total_edge_switches)
         self._host_init(self.total_hosts)
+        self._load_pods()
         self._connect()
+
+    def _load_pods(self):
+        self.pods = []
+        for i in range(0,self.total_edge_switches-1,2):
+            pod = [self.aggregation_layer[i], self.aggregation_layer[i+1],\
+                self.edge_layer[i], self.edge_layer[i+1]]
+            self.pods.append(pod)
 
     def add_switch(self, LAYER, total) -> list:
         layer = None
         if LAYER == 'CORE':
             list(map(lambda x: self.addSwitch('SC'+str(x)),range(1,total+1)))
+            list(map(lambda x: self.core_layer.append('SC'+str(x)),range(1, total+1)))
+
         elif LAYER == 'AGGREGATION':
             list(map(lambda x: self.addSwitch('SA'+str(x)), range(1, total+1)))
-        else:  
+            list(map(lambda x: self.aggregation_layer.append('SA'+str(x)),range(1, total+1)))
+        
+        elif LAYER == 'EDGE':
             list(map(lambda x: self.addSwitch('SE'+str(x)), range(1, total+1)))
+            list(map(lambda x: self.edge_layer.append('SE'+str(x)),range(1, total+1)))
 
     def _core_init(self, total):
-        logger.debug("[%] Core layer init [%]")
         self.add_switch('CORE', total)
 
     def _aggregation_init(self, total):
-        logger.debug("[%] Aggregation layer init [%]")
         self.add_switch('AGGREGATION', total)
 
     def _edge_init(self, total):
-        logger.debug("[%] Edge layer init [%]")
         self.add_switch('EDGE', total)
 
     def _host_init(self, total):
-        logger.debug("[%] Hosts init [%]")
         list(map(lambda x: self.host_layer.append(
             'h'+str(x)), range(1, self.total_hosts+1)))
         list(map(lambda x: self.addHost(x), self.host_layer))   
@@ -87,8 +96,18 @@ class Fat3(mininet.topo.Topo):
         list(map(lambda x: self.addLink('SC4',x), even))       
 
     def _connect_aggregation_to_edge(self):
-        pass
-
+        for pod in self.pods:
+            self.addLink(pod[0],pod[2])
+            self.addLink(pod[0],pod[3])
+            self.addLink(pod[1],pod[2])
+            self.addLink(pod[1],pod[3])
+  
     def _connect_edge_to_hosts(self):
-        pass
+        pointer = 0
+        import pdb;pdb.set_trace()
+        for edge_switch in self.edge_layer:
+            self.addLink(edge_switch, self.host_layer[pointer])
+            self.addLink(edge_switch, self.host_layer[pointer+1])
+            pointer+=2
+
 topos = {'fat3': (lambda: Fat3(4, 2))}
