beb1c9d2ede83fb2414f0cff871a28bb8d9c1253
dalmoveras <d.verasnt@gmail.com>
2024-02-21 06:21:25 -0800

    flow info

---
+++ mininet/fat3/dump-flows.sh
@@ -0,0 +1,15 @@
+sudo ovs-ofctl dump-flows SE1 &> SE1.txt
+sudo ovs-ofctl dump-flows SA1 &> SA1.txt
+sudo ovs-ofctl dump-flows SE2 &> SE2.txt
+sudo ovs-ofctl dump-flows SA2 &> SA2.txt
+sudo ovs-ofctl dump-flows SC4 &> SC4.txt
+sudo ovs-ofctl dump-flows SA8 &> SA8.txt
+sudo ovs-ofctl dump-flows SE8 &> SE8.txt
+sudo ovs-ofctl dump-flows SE3 &> SE3.txt
+sudo ovs-ofctl dump-flows SA4 &> SA4.txt
+sudo ovs-ofctl dump-flows SC3 &> SC3.txt
+sudo ovs-ofctl dump-flows SA6 &> SA6.txt
+sudo ovs-ofctl dump-flows SE5 &> SE5.txt
+sudo ovs-ofctl dump-flows SE7 &> SE7.txt
+sudo ovs-ofctl dump-flows SA7 &> SA7.txt
+
